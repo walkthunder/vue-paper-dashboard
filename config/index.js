72be5ac6ed1df0c03c4a4fb4c2786cc88c3fbb8c
zhengpenghui <zhengpenghui@qingting.fm>
2017-09-13 10:53:07 +0800

    feat(config): Config build and dev env

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
     env: require('./prod.env'),
     srv: path.resolve(__dirname, '../dist'),
     index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '',
     productionSourceMap: true,
