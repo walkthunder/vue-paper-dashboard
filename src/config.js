3221313637ca8bf19abc6ccbe30c1eb3cb474211
zhengpenghui <zhengpenghui@qingting.fm>
2017-09-18 13:49:55 +0800

    feat(static): Set static files.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BACKEND_HOST = 'http://localhost:8011'
+export const BACKEND_HOST = 'http://115.29.5.46:8011'
 export const CMS_HOST = process.env.CMS_HOST
 export const U2_HOST = 'https://u2.qingting.fm'
 export const U2_HOST_TEST = 'http://114.215.242.136:7777'
