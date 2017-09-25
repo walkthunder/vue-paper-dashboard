02a529e9d90eeef90b5fe54b6645b5cc7ce3092a
zhengpenghui <zhengpenghui@qingting.fm>
2017-09-25 19:43:43 +0800

    feat(Pay): Request pay backend with production mode

--- src/config.js
+++ src/config.js
@@ -5,3 +5,4 @@ export const U2_HOST_TEST = 'http://114.215.242.136:7777'
 export const U2_HOST_ONLINE = 'https://u2.qingting.fm'
 export const DAL_HOST = process.env.DAL_HOST // Server side
 export const PAY_HOST_TEST = 'https://test.pay.qingting.fm'
+export const PAY_HOST = 'https://pay.qingting.fm'
